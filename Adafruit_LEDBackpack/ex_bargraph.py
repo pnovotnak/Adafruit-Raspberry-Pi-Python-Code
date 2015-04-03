a5ae13e2b51daf054ad07a6d14441921c4b5b0a8
Peter Novotnak <pnovotnak@flexion.us>
2015-04-02 17:15:24 -0700

    indentaion cleanup

--- Adafruit_LEDBackpack/ex_bargraph.py
+++ Adafruit_LEDBackpack/ex_bargraph.py
@@ -12,8 +12,8 @@ bargraph = Bargraph(address=0x70)
 print "Press CTRL+C to exit"
 
 while(True):
-  for color in range(1, 4):
-    for i in range(24):
-      print i
-      bargraph.setLed(i, color)
-      time.sleep(0.05)
+    for color in range(1, 4):
+        for i in range(24):
+            print i
+            bargraph.setLed(i, color)
+            time.sleep(0.05)
